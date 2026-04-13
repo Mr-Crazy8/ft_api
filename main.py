3f62c31bf236f6743363628bf65f0a72f4cef89e
anas <anas@localhost.localdomain>
2026-04-13 23:54:27 +0100

    0

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from  pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None 
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my api"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your posts"}
+
+
+@app.post("/createposts")
+def creat_posts(post: Post):
+        print(post)
+        print(post.dict())
+        return {"data": post}
